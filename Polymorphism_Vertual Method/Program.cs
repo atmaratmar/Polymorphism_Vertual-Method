6ca7a9d57c98f0880980cd92740063c0e9a11320
atma0004 <atma0004@edu.eal.dk>
2017-05-28 20:19:42 +0200

    Add project files.

---
+++ Polymorphism_Vertual Method/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Vertual_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Transformer trans = new Car();
+            trans.Run();
+        }
+    }
+}
---
+++ Polymorphism_Vertual Method/Transformer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Vertual_Method
+{
+    class Transformer
+    {
+        public virtual void Run()// virtual mean dont run it now and if it is overrid
+                                 //in the child class or not // if it is overrid it will excute that othrwis
+                                 //it will came back and excute parent class 
+        {
+            Console.WriteLine("Run transformer");
+        }
+    }
+    class Car : Transformer
+    {
+        public override void Run()
+        {
+            Console.WriteLine("Run Car");
+        }
+    }
+}
